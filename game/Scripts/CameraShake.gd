1a6d6c4b152e98c18929476113998b431db672db
Pavel <pavelpetrochenko2001@gmail.com>
2022-01-08 01:45:28 +0300

    Added camera shake

---
+++ game/Scripts/CameraShake.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+export var camera_shake_amount = 3
+export var timeShake = 0.3
+var elapsedtime = 0
+
+
+func _process(delta):
+	shake(delta)
+
+
+func shake_on():
+	elapsedtime = timeShake
+
+
+func shake(delta):
+	if elapsedtime > 0:
+		set_offset(Vector2( \
+			rand_range(-1.0, 1.0) * camera_shake_amount, \
+			rand_range(-1.0, 1.0) * camera_shake_amount \
+		))
+		elapsedtime -= delta
